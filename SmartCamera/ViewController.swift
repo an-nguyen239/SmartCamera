9ecbe84d4f2262321be3ce6f5492fb96917cc7ca
An Nguyen <anguyen108@student.gsu.edu>
2018-10-06 00:05:28 -0400

    camera capture output frame

--- SmartCamera/ViewController.swift
+++ SmartCamera/ViewController.swift
@@ -8,14 +8,16 @@
 
 import UIKit
 import AVKit
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         let captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
         
         guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
         
@@ -29,6 +31,17 @@ class ViewController: UIViewController {
         view.layer.addSublayer(previewLayer)
         previewLayer.frame = view.frame
         
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        captureSession.addOutput(dataOutput)
+        
+
+    }
+        //     VNImageRequestHandler(cgImage: T##CGImage, options: [:]).perform(T##requests: [VNRequest]##[VNRequest])
+
+        
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        print("Camera captured a frame: ", Date())
     }
 
 
