ee8f0cf3b735a4c5e6045042e615032c11cf64f3
An Nguyen <anguyen108@student.gsu.edu>
2018-10-05 23:23:30 -0400

    access to phone camera

---
+++ SmartCamera/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  SmartCamera
+//
+//  Created by An Nguyen on 10/5/18.
+//  Copyright © 2018 An Nguyen. All rights reserved.
+//
+
+import UIKit
+import AVKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let captureSession = AVCaptureSession()
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        
+        captureSession.addInput(input)
+        
+        captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
+        
+    }
+
+
+}
+
